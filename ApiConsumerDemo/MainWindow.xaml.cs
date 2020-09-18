50b62c400825c68da1d594d294a389b930683900
Nicolas Bourré <nbourrereg@hotmail.com>
2020-09-18 11:22:48 -0400

    Bypassing MVVM to show new window. IRL develop a windowing service

--- ApiConsumerDemo/MainWindow.xaml.cs
+++ ApiConsumerDemo/MainWindow.xaml.cs
@@ -26,63 +26,20 @@ namespace ApiConsumerDemo
             //nextImageButton.IsEnabled = false;
         }
 
-        //private async Task LoadImage(int imageNumber = 0)
-        //{
-        //    var comic = await ComicProcessor.LoadComic(imageNumber);
-
-        //    if (imageNumber == 0)
-        //    {
-        //        maxNumber = comic.Num;
-        //    }
-
-        //    currentNumber = comic.Num;
-
-        //    var uriSource = new Uri(comic.Img, UriKind.Absolute);
-
-        //    comicImage.Source = new BitmapImage(uriSource, 
-        //        new RequestCachePolicy(RequestCacheLevel.CacheIfAvailable));
-
-        //}
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             vm.LoadImageCommand.Execute(0);
         }
 
-        //private async void previousImageButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (currentNumber > 1)
-        //    {
-        //        currentNumber -= 1;
-        //        nextImageButton.IsEnabled = true;
-        //        await LoadImage(currentNumber);
-
-        //        if (currentNumber == 1)
-        //        {
-        //            previousImageButton.IsEnabled = false;
-        //        }
-        //    }
-        //}
-
-        //private void sunInformationButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    SunInfo sunInfo = new SunInfo();
-        //    sunInfo.Show();
-        //}
-
-        //private async void nextImageButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (currentNumber < maxNumber)
-        //    {
-        //        currentNumber += 1;
-        //        previousImageButton.IsEnabled = true;
-        //        await LoadImage(currentNumber);
-
-        //        if (currentNumber == maxNumber)
-        //        {
-        //            nextImageButton.IsEnabled = false;
-        //        }
-        //    }
-        //}
+        /// <summary>
+        /// Bypassing the MVVM pattern, because it's just a demo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sunInformationButton_Click(object sender, RoutedEventArgs e)
+        {
+            SunInfo sunInfo = new SunInfo();
+            sunInfo.Show();
+        }
     }
 }
